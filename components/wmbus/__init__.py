c5f33d268520f88fb6b9bb13526ea8a7310a34b5
pmichniewski <pmichniewski@gmail.com>
2025-09-03 14:31:48 +0200

    Added library names for cc1101-driver-lib and wmbus-drivers

--- components/wmbus/__init__.py
+++ components/wmbus/__init__.py
@@ -119,13 +119,13 @@ async def to_code(config):
     cg.add_library("SPI", None)
 
     cg.add_library(
-        None,
+        "CC1101-Driver-Lib",
         None,
         "https://github.com/LSatan/SmartRC-CC1101-Driver-Lib#master",
     )
 
     cg.add_library(
-        None,
+        "wmbus-drivers",
         None,
         "https://github.com/pmichniewski/wmbus-drivers",
     )
